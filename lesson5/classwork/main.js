d1f9f16789a68b743ceb3c2fac55ac004517a3fa
ViktorBed <vbednarskii@gmail.com>
2023-08-08 01:43:24 +0300

    commit

---
+++ lesson5/classwork/main.js
@@ -0,0 +1,49 @@
+// const a = "Hellow World";
+// console.log(a);
+
+// let b = 3;
+// b = 4;
+// console.log(b);
+
+// for (let i = 0; index < 10; i++) {
+//   let a = 0;
+// }
+// console.log(a);
+
+// function sum(a, b) {
+//   const res = a - b;
+//   return res;
+// }
+// const res = sum(123, 5);
+// console.log(res);
+
+// function sum(c, d) {
+//   const res1 = c * d;
+//   return res1;
+// }
+// const res1 = sum(5, 8);
+// console.log(res1);
+//100 2
+
+// let multi = (x, y) => x * y;
+// console.log(multi(5, 8));
+
+// function divide(p, o) {
+//   let div = p / o;
+//   return div;
+// }
+// console.log(divide(100, 2));
+
+// let cycle = 1;
+// for (let i = 0; i <= 5; i++) {
+//   console.log((cycle += i));
+// }
+
+// alert("Hellow world");
+
+// setTimeout(() => {
+//   alert("24242");
+// }, 3000);
+
+let res = prompt("скільки рок", 10);
+console.log(`ви запитали рока ${res}`);
