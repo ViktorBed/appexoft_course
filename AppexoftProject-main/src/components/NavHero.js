53d58ad9406b5c49fa8e1c41a8c3f6117552d20e
ViktorBed <vbednarskii@gmail.com>
2023-09-13 00:37:22 +0300

    NavBar

---
+++ AppexoftProject-main/src/components/NavHero.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function NavHero() {
+    return (
+        <h1 className={"text-7xl"}>NavBar</h1>
+    );
+}
+
+export default NavHero;
---
+++ text/my-project/src/Component/NavHero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NavHero() {
+    return (
+        <>
+            <div className={"bg-black w-full"}>
+
+            </div>
+        </>
+    );
+}
+
+export default NavHero;
